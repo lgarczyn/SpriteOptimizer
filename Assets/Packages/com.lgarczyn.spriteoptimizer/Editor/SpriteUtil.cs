45d3a6bdd1586f0c9fa35a65e3ad5a5d4badac07
Lou Garczynski <lgarczyn@student.42.fr>
2025-09-01 22:09:50 +0200

    Added helper to convert unity vertices

---
+++ Assets/Packages/com.lgarczyn.spriteoptimizer/Editor/SpriteUtil.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public static class SpriteUtil
+{
+    public static Vector2[] GetVerticesFinalCoordinates(this Sprite sprite)
+    {
+        Vector2 pivot = sprite.pivot;
+        float ppu = sprite.pixelsPerUnit;
+        Vector2[] vertices = sprite.vertices;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] = vertices[i] * ppu + pivot;
+        }
+        return vertices;
+    }
+}
